68c21195c6abbcd39178dc5890097e16be8cb693
Sterre <xsterrestorm@outlook.com>
2023-12-01 19:34:24 +0100

    Test

---
+++ Day01.py
@@ -0,0 +1,29 @@
+vertaaltabel = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9"
+    }
+def first_digit(line, rev=False):
+    for i, digit in enumerate(line):
+        if digit.isdigit(): return digit
+        for word in vertaaltabel:
+            revword = word[::-1]
+            if digit == word[0] or digit == revword[0]:
+                if line[i:i+len(word)] == word or line[i:i+len(word)] == revword:
+                    return vertaaltabel[word]
+def main(filename):
+    with open(filename) as inp:
+        total = 0
+        for line in inp:
+            combi_int = int(first_digit(line) + first_digit(line[::-1]))
+            print(combi_int)
+            total += combi_int
+    print(total)
+
+main("input/day01.txt")
